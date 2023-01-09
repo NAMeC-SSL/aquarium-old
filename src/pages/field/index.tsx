739941db70c3ce0d4166fcd85470308182f29e29
Etienne Schmitz <contact@etienne-schmitz.com>
2023-01-09 01:59:57 +0100

    fix - center_radius

--- src/pages/field/index.tsx
+++ src/pages/field/index.tsx
@@ -21,15 +21,16 @@ function init_canvas(context: CanvasRenderingContext2D) {
     let x = (0.8 * context.canvas.width) / field.length
     let y = (0.8 * context.canvas.height) / field.width
 
-    let scale_factor = (x+y) / 2;
+    let scale_factor = (x + y) / 2;
 
-    context.translate(context.canvas.width / 2  , context.canvas.height / 2);
+    context.translate(context.canvas.width / 2, context.canvas.height / 2);
 
     context.scale(scale_factor, -scale_factor);
 
-   // context.scale(1,-1);
+    // context.scale(1,-1);
 }
-function draw_shape(ctx, x,y, orientation) {
+
+function draw_shape(ctx, x, y, orientation) {
     ctx.beginPath();
     ctx.arc(
         x,
@@ -43,8 +44,7 @@ function draw_shape(ctx, x,y, orientation) {
 }
 
 
-
-function draw_text(ctx, x,y, id) {
+function draw_text(ctx, x, y, id) {
     ctx.save();
     ctx.translate(
         x,
@@ -54,11 +54,12 @@ function draw_text(ctx, x,y, id) {
     ctx.fillStyle = 'black';
     ctx.font = '1px serif';
     ctx.scale(0.15, -0.15);
-    ctx.fillText(id, -0.085 * 4 , 4 * 0.085);
+    ctx.fillText(id, -0.085 * 4, 4 * 0.085);
 
     ctx.restore();
 
 }
+
 function draw_robots(context) {
     opponents.forEach((opponent) => {
         context.fillStyle = "#dbd81d";
@@ -77,13 +78,13 @@ function draw_penalty(ctx) {
     ctx.strokeRect((field.length / 2) - field.penalty_depth, -field.penalty_width / 2, field.penalty_depth, field.penalty_width);
 
     // Right
-    ctx.strokeRect(- (field.length / 2), - field.penalty_width / 2, field.penalty_depth, field.penalty_width);
+    ctx.strokeRect(-(field.length / 2), -field.penalty_width / 2, field.penalty_depth, field.penalty_width);
 
 }
 
 function draw_goal(ctx) {
     ctx.strokeRect(field.length / 2, -field.goal_width / 2, field.goal_depth, field.goal_width);
-    ctx.strokeRect(-(field.length / 2) - field.goal_depth, - field.goal_width / 2, field.goal_depth, field.goal_width);
+    ctx.strokeRect(-(field.length / 2) - field.goal_depth, -field.goal_width / 2, field.goal_depth, field.goal_width);
 }
 
 function draw_ball(ctx) {
@@ -98,24 +99,32 @@ function draw_ball(ctx) {
 }
 
 function clear_canvas(context: CanvasRenderingContext2D) {
-    context.clearRect(0,0, context.canvas.width, context.canvas.height);
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 }
 
 function draw_line_vertical(ctx) {
     ctx.beginPath();
     ctx.moveTo(0, field.width / 2);
-    ctx.lineTo(0, - field.width / 2);
+    ctx.lineTo(0, -field.width / 2);
     ctx.stroke();
     ctx.closePath();
 }
+
+function draw_center(ctx) {
+    ctx.beginPath();
+    ctx.arc(0, 0, field.center_radius, 0, 2 * Math.PI, false);
+    ctx.stroke();
+    ctx.closePath();
+}
+
 function Field() {
     // @ts-ignore
     field = useSelector((state: RootState) => state.field.field);
     allies = useSelector((state: RootState) => state.robots.allies);
-    opponents = useSelector((state : RootState) => state.robots.opponents);
+    opponents = useSelector((state: RootState) => state.robots.opponents);
     ball = useSelector((state: RootState) => state.ball.ball);
 
-    const canvasRef : Ref<HTMLCanvasElement> = useRef(null);
+    const canvasRef: Ref<HTMLCanvasElement> = useRef(null);
 
     useEffect(() => {
         const id = setInterval(async () => {
@@ -132,17 +141,17 @@ function Field() {
 
             context.strokeRect(-field.length / 2, -field.width / 2, field.length, field.width);
 
-            draw_robots(context);
-
             draw_goal(context);
             draw_penalty(context);
             draw_line_vertical(context);
+            draw_center(context);
 
+            draw_robots(context);
             draw_ball(context);
 
         }, 16);
         return () => clearInterval(id);
-       // context.strokeRect(0, 0, x * 9, y * 6)
+        // context.strokeRect(0, 0, x * 9, y * 6)
     }, []);
 
     return (
